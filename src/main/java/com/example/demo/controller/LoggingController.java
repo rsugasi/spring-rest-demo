8cab8d0da3d6db47a02b66656c7c2be9f51d80f8
rsugasi <sugasi.ramchandra@gmail.com>
2019-09-05 05:36:32 -0400

    adding async log4j2 configuration

--- src/main/java/com/example/demo/controller/LoggingController.java
+++ src/main/java/com/example/demo/controller/LoggingController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import lombok.extern.log4j.Log4j2;
+import org.apache.logging.log4j.ThreadContext;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,11 +19,12 @@ public class LoggingController {
         // log.info("An INFO Message");
         // log.warn("A WARN Message");
         // log.error("An ERROR Message");
-
+        ThreadContext.put("ipAddress", "192.168.21.9");
         Map<String, String> map = new HashMap<>();
         map.put("hello", "world");
         // log.info("{\"hello\": \"world\"}");
         log.info(map);
+        ThreadContext.clearMap();
         return "Howdy! Check out the Logs to see the output...";
     }
 }
