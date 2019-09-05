df69c38beb6e0d673040e3a87010d329b44361a9
rsugasi <sugasi.ramchandra@gmail.com>
2019-09-04 23:47:29 -0400

    Initial Commit

---
+++ src/main/java/com/example/demo/controller/LoggingController.java
@@ -0,0 +1,28 @@
+package com.example.demo.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+import java.util.HashMap;
+
+@RestController
+@Log4j2
+public class LoggingController {
+
+    @RequestMapping("/")
+    public String index() {
+        // log.trace("A TRACE Message");
+        // log.debug("A DEBUG Message");
+        // log.info("An INFO Message");
+        // log.warn("A WARN Message");
+        // log.error("An ERROR Message");
+
+        Map<String, String> map = new HashMap<>();
+        map.put("hello", "world");
+        // log.info("{\"hello\": \"world\"}");
+        log.info(map);
+        return "Howdy! Check out the Logs to see the output...";
+    }
+}
